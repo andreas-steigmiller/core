c0539fe9302044ca82d4800125d13be0da17836b
Sarunas Marciuska <marciuska.sarunas@gmail.com>
2015-01-15 12:23:41 +0000

    Added comments for ranking classes

--- src/semfacet/ranking/Ranking.java
+++ src/semfacet/ranking/Ranking.java
@@ -1,3 +1,7 @@
+/**
+ * This class was created to rank facet names and facet values using some objective rankings i.e. alphabetically. One could extend this class to add different rankings if needed. 
+ */
+
 package semfacet.ranking;
 
 import java.util.Collections;
@@ -9,35 +13,35 @@ import semfacet.data.structures.FacetValue;
 
 public class Ranking {
     public static void sortFacetNamesAlphabetically(List<FacetName> facetNames, final boolean ascOrder) {
-	if (facetNames.size() > 0) {
-	    Collections.sort(facetNames, new Comparator<FacetName>() {
-		public int compare(final FacetName v1, final FacetName v2) {
-		    if (ascOrder)
-			return v1.getName().compareTo(v2.getName());
-		    else
-			return v2.getName().compareTo(v1.getName());
-		}
-	    });
-	}
+        if (facetNames.size() > 0) {
+            Collections.sort(facetNames, new Comparator<FacetName>() {
+                public int compare(final FacetName v1, final FacetName v2) {
+                    if (ascOrder)
+                        return v1.getName().compareTo(v2.getName());
+                    else
+                        return v2.getName().compareTo(v1.getName());
+                }
+            });
+        }
     }
 
     public static void sortFacetValuesAlphabetically(List<FacetValue> facetValues) {
-	if (facetValues.size() > 0) {
-	    Collections.sort(facetValues, new Comparator<FacetValue>() {
-		public int compare(final FacetValue v1, final FacetValue v2) {
-		    return v1.getLabel().compareTo(v2.getLabel());
-		}
-	    });
-	}
+        if (facetValues.size() > 0) {
+            Collections.sort(facetValues, new Comparator<FacetValue>() {
+                public int compare(final FacetValue v1, final FacetValue v2) {
+                    return v1.getLabel().compareTo(v2.getLabel());
+                }
+            });
+        }
     }
-    
+
     public static void sortFacetValuesByRank(List<FacetValue> facetValues) {
-	if (facetValues.size() > 0) {
-	    Collections.sort(facetValues, new Comparator<FacetValue>() {
-		public int compare(final FacetValue v1, final FacetValue v2) {
-		    return v2.getRanking() - v1.getRanking();
-		}
-	    });
-	}
+        if (facetValues.size() > 0) {
+            Collections.sort(facetValues, new Comparator<FacetValue>() {
+                public int compare(final FacetValue v1, final FacetValue v2) {
+                    return v2.getRanking() - v1.getRanking();
+                }
+            });
+        }
     }
 }
--- src/semfacet/ranking/RankingMap.java
+++ src/semfacet/ranking/RankingMap.java
@@ -1,3 +1,7 @@
+/**
+ * This class should retrieve various information related to facet ranking based on user activity. Maybe it should be renamed. Currently, it supports very basic methods. 
+ */
+
 package semfacet.ranking;
 
 import java.sql.ResultSet;
@@ -8,19 +12,20 @@ import semfacet.data.structures.Configurations;
 
 public class RankingMap {
     public static Map<String, Integer> getRankingMap(Configurations config, String facetName, String keywords) {
-	Map<String, Integer> map = new HashMap<String, Integer>();
-	try {
-	    String query = "SELECT facet_value, count(*) FROM user_activity WHERE facet_name ='" + facetName + "' AND keywords ='" + keywords + "' GROUP BY facet_value";
-	    ResultSet rs = config.getActivityDatabase().executeQuery(query);
-	    for (; rs.next();) {
-		String facetValue = rs.getString(1);
-		Integer ranking = rs.getInt(2);
-		map.put(facetValue, ranking);
-	    }
-	} catch (Exception e) {
-	    e.printStackTrace();
-	}
-	return map;
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        try {
+            String query = "SELECT facet_value, count(*) FROM user_activity WHERE facet_name ='" + facetName + "' AND keywords ='" + keywords
+                    + "' GROUP BY facet_value";
+            ResultSet rs = config.getActivityDatabase().executeQuery(query);
+            for (; rs.next();) {
+                String facetValue = rs.getString(1);
+                Integer ranking = rs.getInt(2);
+                map.put(facetValue, ranking);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return map;
     }
 
 }
